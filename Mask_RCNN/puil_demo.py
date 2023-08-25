bf3eab3c70b5a1735f5e4fc8b891d52dacb99dfa
sabrinawei0226 <sabrina0226wei@gmail.com>
2023-08-25 22:11:38 +0800

    Create puil_demo.py

---
+++ Mask_RCNN/puil_demo.py
@@ -0,0 +1,81 @@
+import os
+import sys
+import random
+import math
+import numpy as np
+import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn import utils
+import mrcnn.model as modellib
+from mrcnn import visualize
+# Import COCO config
+sys.path.append(os.path.join(ROOT_DIR, "C:/pythonwork/shuo/Mask_RCNN-master/"))  # To find local version
+print(ROOT_DIR)
+import samples.pupil.pupil_n
+
+#% matplotlib inline 
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "C:/pythonwork/shuo/Mask_RCNN-master/logs/mask_rcnn_catvsdog_0030.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+
+# Directory of images to run detection on
+IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+
+class InferenceConfig(samples.pupil.pupil_n.PointConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+config = InferenceConfig()
+config.display()
+
+# Create model object in inference mode.
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+# Load weights trained on MS-COCO
+model.load_weights(COCO_MODEL_PATH, by_name=True)
+
+# COCO Class names
+# Index of the class in the list is its ID. For example, to get ID of
+# the teddy bear class, use: class_names.index('teddy bear')
+class_names = ['BG','ca','tumor']
+
+# Load a random image from the images folder
+#file_names = next(os.walk(IMAGE_DIR))[2]
+count = os.listdir(IMAGE_DIR)
+for i in range(0,len(count)):
+    path = os.path.join(IMAGE_DIR, count[i])
+    if os.path.isfile(path):
+        file_names = next(os.walk(IMAGE_DIR))[2]
+        image = skimage.io.imread(os.path.join(IMAGE_DIR, count[i]))
+        # Run detection
+        results = model.detect([image], verbose=1)
+        r = results[0]
+        visualize.display_instances(count[i],image, r['rois'], r['masks'], r['class_ids'], 
+                            class_names, r['scores'])
+
+# Visualize results
+r = results[0]
+visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], 
+                            class_names, r['scores'])
+
+#plt.savefig("D:/Users/user/Desktop/maskRCNN/sav.jpg")
+
+							
+							
+							
+							
